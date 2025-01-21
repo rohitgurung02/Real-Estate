778270c348c2f7da3d73f39f8bb43d4a7d2c30d9
unknown <rohitgurung086@gmail.com>
2025-01-21 16:00:48 +0530

    update the route page

--- src/lib/mongoDb/mongoose.js
+++ src/lib/mongoDb/mongoose.js
@@ -12,7 +12,7 @@ export const connect = async () => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "real-estate",
+      dbName: "next-estate",
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
